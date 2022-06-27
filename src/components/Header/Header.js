636289caf2655c85788dc51ffb8cc10de9638791
h-loic <h-loic@orange.fr>
2022-06-28 01:05:55 +0200

    added menu toggle button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,11 +10,22 @@ export default function Header() {
 		<div className="responsive-container">
 			<h1>Loïc Hug</h1>
             <nav>
-                <a href="/">
+                <a href="/" className='nav-item'>
                     <BsGithub size={28} className='center'/>
                 </a>
-                <a href="/">
+                <a href="/" className='nav-item'>
                     <BsLinkedin size={28} className='center'/>
+                </a>
+                <a className='center nav-button' href='/'>
+                    <input type="checkbox" id="menu-toggle"/>
+                    <label for="menu-toggle" className="hamburger center">
+                        <span className="bun bun-top">
+                            <span className="bun-crust bun-crust-top"></span>
+                        </span>
+                        <span className="bun bun-bottom">
+                            <span className="bun-crust bun-crust-bottom"></span>
+                        </span>
+                    </label>
                 </a>
             </nav>
 		</div>
