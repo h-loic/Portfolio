cec3a6ec7dc99f62375a53ed95ff052913e16e9c
h-loic <h-loic@orange.fr>
2022-06-28 11:29:20 +0200

    added full screen menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.scss'
 import {BsGithub} from 'react-icons/bs'
 import {BsLinkedin} from 'react-icons/bs'
 
 export default function Header() {
-  return (
-    <header className="header">
-        <div className='header-margin'></div>
-		<div className="responsive-container">
-			<h1>Loïc Hug</h1>
-            <nav>
-                <a href="/" className='nav-item'>
-                    <BsGithub size={28} className='center'/>
-                </a>
-                <a href="/" className='nav-item'>
-                    <BsLinkedin size={28} className='center'/>
-                </a>
-                <a className='center nav-button' href='/'>
-                    <input type="checkbox" id="menu-toggle"/>
-                    <label for="menu-toggle" className="hamburger center">
-                        <span className="bun bun-top">
-                            <span className="bun-crust bun-crust-top"></span>
-                        </span>
-                        <span className="bun bun-bottom">
-                            <span className="bun-crust bun-crust-bottom"></span>
-                        </span>
-                    </label>
-                </a>
-            </nav>
-		</div>
-	</header>
-  )
+
+    const [isOpen,setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <>
+            <header className="header">
+                <div className='header-margin'></div>
+                <div className="responsive-container">
+                    <h1>Loïc Hug</h1>
+                    <nav>
+                        <a href="/" className='nav-item'>
+                            <BsGithub size={28} className='center'/>
+                        </a>
+                        <a href="/" className='nav-item'>
+                            <BsLinkedin size={28} className='center'/>
+                        </a>
+                        <a className='center nav-button' href='/'>
+                            <input type="checkbox" id="menu-toggle" onClick={toggleMenu}/>
+                            <label htmlFor="menu-toggle" className="hamburger center">
+                                <span className="bun bun-top">
+                                    <span className="bun-crust bun-crust-top"></span>
+                                </span>
+                                <span className="bun bun-bottom">
+                                    <span className="bun-crust bun-crust-bottom"></span>
+                                </span>
+                            </label>
+                        </a>
+                    </nav>
+                </div>
+            </header>
+                <div id="myNav" class="overlay"         
+                    style={{
+                        height : isOpen ? "100%" : "0%"
+                    }}>
+                    <div class="overlay-content">
+                        <a href="/">Mes projets</a>
+                        <a href="/">Mes compétences</a>
+                        <a href="/">A propos de moi</a>
+                        <a href="/">Me contacter</a>
+                    </div>
+                </div>
+        </>
+    )
 }
